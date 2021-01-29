150290a2caf3cde5e401d4464a593478b4f04199
atara1 <atarabu49@gmail.com>
2021-01-29 17:10:58 +0200

    change function name

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,7 +1,7 @@
 import { Time, TimerState } from './../../../assets/timeType';
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
-
+const LOCAL_STORAGE_TIMER_KEY = 'timer';
 const DEFAULT_TIMER_STATE: TimerState = {
   counter: 0,
   currentTime: { minutes: '00', seconds: '00', milliseconds: '00' },
@@ -28,46 +28,50 @@ export class TimerComponent implements OnInit, OnDestroy {
     this.loadDataFromLocalStorage();
     if (!this.isPaused) {
       const lastPlayTimestamp = Date.now() - this.lastPlayTimestamp;
-      this.addTimeAfterUserReturns(lastPlayTimestamp);
-      this.startTimer();
+      this.addTimeAfterUsersReturns(lastPlayTimestamp);
+      this.calculateTimer();
     }
   }
 
-  addTimeAfterUserReturns(lastPlayTimestamp: number): void {
+  addTimeAfterUsersReturns(lastPlayTimestamp: number): void {
     this.counter += lastPlayTimestamp;
   }
 
-  startTimer(): void {
+  calculateTimer(): void {
     const startTime = Date.now() - (this.counter || 0);
     let lastPlayTimestampToAdd = this.counter;
     this.timerRef = setInterval(() => {
       this.counter = Date.now() - startTime;
-      const counterWithTimestamp = lastPlayTimestampToAdd + this.counter;
-      this.counter = !this.isPaused ? counterWithTimestamp : this.counter;
+      const counterWithLastPlayTimestamp = lastPlayTimestampToAdd + this.counter;
+      this.counter = !this.isPaused ? counterWithLastPlayTimestamp : this.counter;
       this.timer.milliseconds = Math.floor(Math.floor(this.counter % 1000) / 10).toFixed(0);
       this.timer.minutes = (Math.floor(this.counter / 60000)).toString();
       this.timer.seconds = Math.floor(Math.floor(this.counter % 60000) / 1000).toFixed(0);
-      if (Number(this.timer.minutes) < 10) {
-        this.timer.minutes = '0' + this.timer.minutes;
-      } else {
-        this.timer.minutes = '' + this.timer.minutes;
-      }
-      if (Number(this.timer.milliseconds) < 10) {
-        this.timer.milliseconds = '0' + this.timer.milliseconds;
-      } else {
-        this.timer.milliseconds = '' + this.timer.milliseconds;
-      }
-      if (Number(this.timer.seconds) < 10) {
-        this.timer.seconds = '0' + this.timer.seconds;
-      } else {
-        this.timer.seconds = '' + this.timer.seconds;
-      }
+      this.paddingTheTime();
       lastPlayTimestampToAdd = 0;
     }, 10);
   }
 
+  paddingTheTime(): void {
+    if (Number(this.timer.minutes) < 10) {
+      this.timer.minutes = '0' + this.timer.minutes;
+    } else {
+      this.timer.minutes = '' + this.timer.minutes;
+    }
+    if (Number(this.timer.milliseconds) < 10) {
+      this.timer.milliseconds = '0' + this.timer.milliseconds;
+    } else {
+      this.timer.milliseconds = '' + this.timer.milliseconds;
+    }
+    if (Number(this.timer.seconds) < 10) {
+      this.timer.seconds = '0' + this.timer.seconds;
+    } else {
+      this.timer.seconds = '' + this.timer.seconds;
+    }
+  }
 
-  addTime(): void {
+
+  addTimer(): void {
     const time: Time = { minutes: this.timer.minutes, seconds: this.timer.seconds, milliseconds: this.timer.milliseconds };
     const isTimerFound = this.isTimerExist(time);
     if (!isTimerFound) {
@@ -84,7 +88,7 @@ export class TimerComponent implements OnInit, OnDestroy {
     return isFound ? true : false;
   }
 
-  timerToRemove(timerToRemove): void {
+  timerToRemove(timerToRemove: Time): void {
     const timerList: Time[] = [...this.listOfTimer];
     timerList.forEach((item, index) => {
       if (item.minutes === timerToRemove.minutes &&
@@ -96,8 +100,8 @@ export class TimerComponent implements OnInit, OnDestroy {
     this.listOfTimer = timerList;
   }
 
-  reset(): void {
-    this.pauseWatch();
+  resetTimer(): void {
+    this.pauseTimer();
     this.timer = { minutes: '00', seconds: '00', milliseconds: '00' };
     this.counter = 0;
     this.listOfTimer = []; // remove all timers
@@ -105,29 +109,25 @@ export class TimerComponent implements OnInit, OnDestroy {
 
   tooglePlay(): void {
     if (this.isPaused) {
-      this.playWatch();
+      this.playTimer();
     }
     else {
-      this.pauseWatch();
+      this.pauseTimer();
     }
   }
 
-  playWatch(): void {
+  playTimer(): void {
     this.isPaused = false;
-    this.updateTimer();
+    this.calculateTimer();
   }
 
-  pauseWatch(): void {
+  pauseTimer(): void {
     this.isPaused = true;
     clearInterval(this.timerRef);
   }
 
-  updateTimer(): void {
-    this.startTimer();
-  }
-
   loadDataFromLocalStorage(): void {
-    const timeState: TimerState = JSON.parse(localStorage.getItem('timer'))
+    const timeState: TimerState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TIMER_KEY))
       || { ...DEFAULT_TIMER_STATE };
     this.timer = timeState.currentTime;
     this.counter = timeState.counter;
@@ -144,9 +144,8 @@ export class TimerComponent implements OnInit, OnDestroy {
       counter: this.counter,
       lastPlayTimestamp: Date.now()
     };
-    localStorage.setItem('timer', JSON.stringify(timerState));
+    localStorage.setItem(LOCAL_STORAGE_TIMER_KEY, JSON.stringify(timerState));
   }
-
 
   @HostListener('window:beforeunload')
   ngOnDestroy(): void {
